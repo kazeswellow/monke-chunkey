5860deea9e9c9f8a06acba6a345c3b80d36e7050
Coyolord <66645689+kazeswellow@users.noreply.github.com>
2022-02-12 11:01:46 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,117 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import { Header } from 'react-native-elements';
+import db from './localdb';
+import PhonicSoundButton from './components/PhonicSoundButton';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      chunks:[],
+      phonicSounds:[],
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header
+          backgroundColor={'#9c8210'}
+          centerComponent={{
+            text: 'Monkey Chunky',
+            style: { color: '#fff', fontSize: 20 },
+          }}
+        />
+        <Image
+          style = {styles.imageIcon}
+          source = {{uri:'https://www.net-aware.org.uk/siteassets/images-and-icons/application-icons/app-icons-monkey.png?w=585&scale=down'}}
+        />
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => {
+            this.setState({ text: text });
+          }}
+          value={this.state.text}
+        />
+        <TouchableOpacity
+          style={styles.goButton}
+          onPress={() => {
+            this.setState({ 
+              chunks:db[this.state.text].chunks
+             });
+             this.setState({ 
+              phonicSounds:db[this.state.text].phones
+             });
+          }}>
+          <Text style={styles.buttonText}>GO</Text>
+        </TouchableOpacity>
+        <View>
+          {this.state.chunks.map((item, index) => {
+            return (
+              <PhonicSoundButton
+                wordChunk={this.state.chunks[index]}
+                soundChunk={this.state.phonicSounds[index]}
+              />
+            );
+          })}
+        </View>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#b8b8b8',
+  },
+  inputBox: {
+    marginTop: 100,
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 4,
+  },
+  goButton: {
+    width: '50%',
+    height: 70,
+    alignSelf: 'center',
+    padding: 10,
+    margin: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  displayText: {
+    textAlign: 'center',
+    fontSize: 30,
+  },
+  imageIcon: {
+    width:170,
+    height:170,
+    marginLeft:120,
+    marginTop:100
+  },
+  chunkButton:{
+    width:'60%',
+    height:50,
+    justifyContent:'center',
+    alignItems:'center',
+    alignSelf:'center',
+    borderRadius:10,
+    margin:5,
+    backgroundColor:'red',
+  }
+});
